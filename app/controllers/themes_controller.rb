9cabe1fe462af483bb779f0d9619755f484ada3f
harapeco-mgn <harapeco.mgn@gmail.com>
2026-01-14 10:08:35 +0900

    RSVP機能の表示を改善し、参加状態に応じたボタンスタイルを適用

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,15 +1,17 @@
 class ThemesController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_theme, only: [:show]
+  
   def index
     @themes = Theme.order(created_at: :desc)
   end
 
   def show
-  @theme = Theme.find(params[:id])
-  @theme_comment  = ThemeComment.new
-  @theme_comments = @theme.theme_comments.includes(:user).order(created_at: :desc)
-end
+    @theme = Theme.find(params[:id])
+    @theme_comment  = ThemeComment.new
+    @theme_comments = @theme.theme_comments.includes(:user).order(created_at: :desc)
+    @rsvp = current_user.rsvps.find_by(theme: @theme) if user_signed_in?
+  end
 
   def new
     @theme = Theme.new
@@ -30,5 +32,9 @@ end
 
   def theme_params
     params.require(:theme).permit(:category, :title, :description)
+  end
+
+  def set_theme
+    @theme = Theme.find(params[:id])
   end
 end
