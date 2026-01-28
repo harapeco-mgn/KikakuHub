686b5095c0af5c96840fe1b607f14c910f334faf
harapeco-mgn <harapeco.mgn@gmail.com>
2026-01-28 14:51:17 +0900

    参加時間集計のカテゴリ切り替えの非同期化

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,8 +3,10 @@ import AutoDismissController from "controllers/auto_dismiss_controller"
 import SecondaryButtonController from "controllers/secondary_button_controller"
 import AvailabilityDraftController from "controllers/availability_draft_controller"
 import AvailabilitySlotsController from "controllers/availability_slots_controller"
+import AutoSubmitController from "controllers/auto_submit_controller"
 
 application.register("auto-dismiss", AutoDismissController)
 application.register("secondary-button", SecondaryButtonController)
 application.register("availability-draft", AvailabilityDraftController)
 application.register("availability-slots", AvailabilitySlotsController)
+application.register("auto-submit", AutoSubmitController)
