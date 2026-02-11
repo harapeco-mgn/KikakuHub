1302b1717a71f78c579ed0205f8e8d5e0fa92326
harapeco-mgn <harapeco.mgn@gmail.com>
2026-02-12 06:17:13 +0900

    pendingモデルスペックを実装 (#153)
    - User: associations, validations, cohort_label, cohort_options のテストを追加
    - Theme: associations, validations, enums, scopes のテストを追加
    - ThemeComment: associations, validations のテストを追加
    - AvailabilitySlot: associations, enums, validations のテストを追加

--- spec/models/availability_slot_spec.rb
+++ spec/models/availability_slot_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe AvailabilitySlot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'enums' do
+    it { is_expected.to define_enum_for(:category).with_values(tech: 0, community: 1) }
+  end
+
+  describe 'validations' do
+    subject { build(:availability_slot) }
+
+    it { is_expected.to validate_presence_of(:category) }
+    it { is_expected.to validate_presence_of(:wday) }
+    it { is_expected.to validate_presence_of(:start_minute) }
+    it { is_expected.to validate_presence_of(:end_minute) }
+  end
 end
