c46b89acfeba1529112715f61378df8a27ab7df8
harapeco-mgn <harapeco.mgn@gmail.com>
2026-02-12 11:59:23 +0900

    Rubocopスタイル修正

--- db/migrate/20260212025837_add_indexes_to_tables.rb
+++ db/migrate/20260212025837_add_indexes_to_tables.rb
@@ -1,7 +1,7 @@
 class AddIndexesToTables < ActiveRecord::Migration[7.2]
   def change
     add_index :availability_slots, :category
-    add_index :availability_slots, [:category, :wday]
+    add_index :availability_slots, [ :category, :wday ]
     add_index :themes, :category
     add_index :themes, :created_at
   end
